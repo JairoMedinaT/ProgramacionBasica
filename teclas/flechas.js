7e52c404d6ef6ea3fa939da14509890d25cd15ef
Jairo Ismael Medina Tarazona. JIMT <jairoismael@hotmail.com>
2023-03-29 21:11:54 -0500

    Dibujo con el Teclado

--- teclas/flechas.js
+++ teclas/flechas.js
@@ -7,25 +7,47 @@ var teclas = {
 };
 
 console.log(teclas);
-document.addEventListener("keyup", dibujarTeclado);
+// document.addEventListener("keyup", dibujarTeclado);
+document.addEventListener("keydown", dibujarTeclado);
+var cuadrito = document.getElementById("area_De_dibujo");
+var papel = cuadrito.getContext("2d");
+var x = 100;
+var y = 100;
+
+dibujarLinea("red", x-1, y-1, x+1, y+1, papel);
+
+function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal, lienzo) {
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.lineWidth = 3;
+    lienzo.moveTo(xInicial, yInicial);
+    lienzo.lineTo(xFinal, yFinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}
 
 function dibujarTeclado(evento) {
     // alert("oh, por Dios");
     // console.log("Tecla oprimida");
     // console.log(evento.keyCode);
-
+    var colorcito = "green";
+    var movimiento = 1;
     switch (evento.keyCode) {
         case teclas.LEFT:
-            console.log("Izquierda");
+            dibujarLinea(colorcito, x, y, x-movimiento, y, papel);
+            x = x - movimiento;
             break;
         case teclas.RIGHT:
-            console.log("Derecha");
+            dibujarLinea(colorcito, x, y, x+movimiento, y, papel);
+            x = x + movimiento;
             break;
         case teclas.UP:
-            console.log("Arriba");
+            dibujarLinea(colorcito, x, y, x, y-movimiento, papel);
+            y = y - movimiento;
             break;
         case teclas.DOWN:
-            console.log("Abajo");
+            dibujarLinea(colorcito, x, y, x, y+movimiento, papel);
+            y = y + movimiento;
             break;
         default:
             console.log("Otra tecla");
