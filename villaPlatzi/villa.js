121997ca092252fef4ce16360eef0246b0e35d26
Jairo Ismael Medina Tarazona. JIMT <jairoismael@hotmail.com>
2023-04-26 19:48:42 -0500

    Dibujar objetos por separado varias funciones de carga

--- villaPlatzi/villa.js
+++ villaPlatzi/villa.js
@@ -1,14 +1,41 @@
-// alert("Hola");
-var z;
+var vp = document.getElementById("villaplatzi");
+var papel = vp.getContext("2d");
+var mapa = "tile.png";
 
-var n = 10;
-for(var i=0; i < n; i++){
-    z = aleatorio(-5, 5);
-    document.write(z + ', ');
+var fondo = new Image();
+fondo.src = mapa;
+fondo.addEventListener("load", dibujarFondo);
+
+var vaca = new Image();
+vaca.src = "vaca.png";
+vaca.addEventListener("load", dibujarVaca);
+
+var cerdo = new Image();
+cerdo.src = "cerdo.png";
+cerdo.addEventListener("load", dibujarCerdo);
+
+var pollo = new Image();
+pollo.src = "pollo.png";
+pollo.addEventListener("load", dibujarPollo);
+
+function dibujarFondo() {
+    papel.drawImage(fondo, 0, 0);
 }
 
-function aleatorio(min, max){
+function dibujarVaca() {
+    papel.drawImage(vaca, 10, 10);
+}
+
+function dibujarCerdo() {
+    papel.drawImage(cerdo, 10, 300);
+}
+
+function dibujarPollo() {
+    papel.drawImage(pollo, 300, 150);
+}
+
+function aleatorio(min, max) {
     var resultado;
-    resultado = Math.floor(Math.random() * (max -min + 1)) + min;
+    resultado = Math.floor(Math.random() * (max - min + 1)) + min;
     return resultado;
 }
